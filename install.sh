a1fe0b70353a9bd9ed7e9b69c489d1e93ec71d3e
____marcell <0000marcell@gmail.com>
2019-03-26 14:26:44 +0000

    testing

---
+++ install.sh
@@ -0,0 +1,6 @@
+# execute curl -s https://raw.githubusercontent.com/0000marcell/scripts/master/install.sh | bash 
+dialog                                     \
+   --title 'Installing Packages!'        \
+   --gauge '\ninstalling nvm'  \
+   8 40 60
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
