fc276e19c677b98a986d63c4a8db4deb8f91b811
____marcell <0000marcell@gmail.com>
2019-12-16 21:43:02 +0000

    install scripts modified

--- install-scripts.sh
+++ install-scripts.sh
@@ -4,6 +4,7 @@ mkdir ~/scripts
 echo 'export PATH="$PATH:$HOME/scripts"' >> ~/.bashrc
 source ~/.bashrc
 CVART=$(curl -s https://api.github.com/repos/0000marcell/scripts/contents | awk '/name/ { print $2 }' | tr -d '"' | tr -d ',')
+CVART=($CVART)
 for i in "${CVART[@]}"; do 
   echo "Installing "$i" ---> ~/scripts/"$i
   curl -s https://raw.githubusercontent.com/0000marcell/scripts/master/$i > ~/scripts/$i
