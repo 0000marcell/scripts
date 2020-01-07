2e4f0ba08b4cbed620768ad63c7c7ae50c7282a5
____marcell <0000marcell@gmail.com>
2020-01-07 14:37:17 +0000

    figma2css install updated

--- install-figma2css.sh
+++ install-figma2css.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
-mkdir ~/scripts
-if [ ! -f "~/.bashrc" ]; then
-  echo "You dont have a .bashrc file in your HOME directory plz verify bash instalation aborting..."
+mkdir ~/scripts 2> /dev/null
+CBASHFILE=~/.bashrc
+if [ ! -f "$CBASHFILE" ]; then
+  echo "You dont have a .bashrc file in your HOME directory plz verify bash instalation, aborting..."
   exit 0
 fi
 echo 'export PATH="$PATH:$HOME/scripts"' >> ~/.bashrc
 source ~/.bashrc
-npm i -g figma2css figmafetch figmaprint figmafind
+sudo npm i -g figma2css figmafetch figmaprint figmafind findinjson
 CVART=(figma2cssgenerate figma2cssinstall figma2cssopen figmadata figmafiles figmafilesraw figmaopen figmaprojectsraw)
 for i in "${CVART[@]}"; do 
   echo "Installing "$i" ---> ~/scripts/"$i
